c108acbf4176706314e482a6f336b7ff063d360b
zshengjun <stupidzhang@aliyun.com>
2022-01-10 23:37:54 +0800

    feat: init project

--- jingfen/src/main/java/com/stupidzhang/jingfen/controller/JingFenController.java
+++ jingfen/src/main/java/com/stupidzhang/jingfen/controller/JingFenController.java
@@ -4,7 +4,6 @@ import com.stupidzhang.jingfen.constant.JdConstants;
 import com.stupidzhang.jingfen.service.JingFenApiService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
